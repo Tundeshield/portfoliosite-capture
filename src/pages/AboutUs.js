f76e5200110a8f704e9aeb93e15c71cb75331ed4
Tundeshield <tundeshield@gmail.com>
2020-12-23 02:40:27 +0100

    3. Services and Faq section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import AboutSection from "../components/AboutSection"
+import AboutSection from "../components/AboutSection";
+import Services from "../components/Services";
+import Faq from "../components/Faq";
+
 
 
 const AboutUs=()=>{
     return(
     <div className="aboutus">
          <AboutSection/>
+         <Services/>
+         <Faq/>
     </div>
     )
 }
